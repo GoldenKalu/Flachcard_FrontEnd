0e05ae80cb8823f2e59c599bd53e3bb4c4e1e77c
Golden Kalu <goldenlemmy@gmail.com>
2021-07-07 20:15:13 -0700

    app.js created

---
+++ my-app/src/App.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import FlashcardList from './flashcardList';
+import './app.css'
+import axios from 'axios'
+
+function App() {
+  const [flashcards, setFlashcards] = useState(sample_flashcards)
+
+  useEffect(() => {
+    const getFlashcards = async () => {
+      try{
+        let response = await axios.get('http://localhost:5000/api/flashcards/')
+        setFlashcards(response.data)
+
+      } catch(err){
+        console.log(err)
+
+      }
+    }
+
+    getFlashcards()
+      
+  }, [])
+  return (
+    <FlashcardList flashcards={flashcards} />
+  );
+}
+
+const sample_flashcards = [
+  {
+    id: 1,
+    question: 'what is JavaScript?',
+    answer: 'a programming language',
+    options: [
+      'a programming language',
+      'a note taking app',
+    ]
+  },
+
+  {
+    id: 2,
+    question: 'Question 2?',
+    answer: 'Answer',
+    options: [
+      'Answer 1',
+      'Answer2',
+    ]
+  },
+  
+
+]
+
+export default App;
